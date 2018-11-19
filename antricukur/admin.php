2e9daa10a15beb0926cc1d6fcd302085d9261332
rajaf123 <44833314+rajaf123@users.noreply.github.com>
2018-11-19 14:28:38 +0700

    Add files via upload

---
+++ antricukur/admin.php
@@ -0,0 +1,111 @@
+ <h4 align="center">Admin</h4>
+ <div class="card">
+  <div class="card-body">
+  	<div class="alert alert-primary" role="alert">
+		Buka
+	</div>
+	<div class="row">
+    <div class="col-sm text-left">
+		<div class="form-check">
+		  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+		  <label class="form-check-label" for="exampleRadios1">
+		    Buka
+		  </label>
+		</div>
+		<div class="form-check">
+		  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+		  <label class="form-check-label" for="exampleRadios2">
+		    Tutup
+		  </label>
+		</div>
+    </div>
+    <div class="col-sm text-right">
+      <a href="" class="btn btn-info">Update</a>
+    </div>
+   </div> 
+  </div>
+ </div>
+
+<div class="shadow p-3 mb-5 bg-white rounded">
+ <div class="card">
+  <div class="card-body text-left">
+  	<i class="fa fa-users"></i> Antrian
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-body text-left">
+<div class="table-responsive">
+  	<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Status</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Bagas</td>
+      <td>Booking</td>
+      <td><a href="" class="btn btn-success"><i class="fa fa-check"></i></a></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Fajar</td>
+      <td>Booking</td>
+      <td><a href="" class="btn btn-success"><i class="fa fa-check"></i></a></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Adit</td>
+      <td>Booking</td>
+      <td><a href="" class="btn btn-success"><i class="fa fa-check"></i></a></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</div>
+
+  </div>
+</div>
+
+
+
+
+<div class="card">
+  <div class="card-body">
+		Tambah Antrian
+	
+		<form>
+  <div class="form-group">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Tambah">
+
+</div></form>
+
+	<div class="row">
+    <div class="col-sm text-left">
+		<div class="form-check">
+		  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+		  <label class="form-check-label" for="exampleRadios1">
+		    Di Tempat
+		  </label>
+		</div>
+		<div class="form-check">
+		  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+		  <label class="form-check-label" for="exampleRadios2">
+		    Booking
+		  </label>
+		</div>
+    </div>
+    <div class="col-sm text-right">
+      <a href="" class="btn btn-info">Nama</a>
+    </div>
+   </div> 
+  </div>
+ </div>
+
+
+  
